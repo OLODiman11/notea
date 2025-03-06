a6bfb3d63c759fc4618c7d863675bf6797268b88
OLODiman11 <OLODiman11@mail.ru>
2025-03-07 01:02:44 +0300

    build: исходная структура

---
+++ src/main/java/by/olodiman11/notea/NoteaApplication.java
@@ -0,0 +1,13 @@
+package by.olodiman11.notea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/olodiman11/notea/NoteaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.olodiman11.notea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
